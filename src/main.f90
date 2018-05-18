5d07941e416b79ff88f8dd6eb31e7657d1ae77f6
Parth Thakkar <path1380@colorado.edu>
2018-05-18 11:21:31 -0600

    Running with MPI and added PetscInitialize. The snail moves forward!

--- src/main.f90
+++ src/main.f90
@@ -1,9 +1,29 @@
   program main
-#include <petsc/finclude/petscksp.h>
-!#include <src/type_defs.f90>
+#include <petsc/finclude/petsc.h>
   use type_defs
+  use petsc
+  use petscvec
   implicit none
-  !call PetscInitialize(character(*) file, integer ierr)
+  
+  PetscInt n
+  PetscErrorCode ierr
+  PetscBool flg
+  PetscScalar one,two,three,dot
+  PetscReal norm,rdot
+  Vec x,y,w
+  PetscOptions options
+
+  n = 20
+  one = 1.0
+  two = 2.0
+  three = 3.0
+
+  call MPI_Init(ierr)
+  call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
+
+  !call PetscOptionsCreate(options,ierr)
+  !call VecCreate(PETSC_COMM_SELF,x,ierr)
+
   write(*,*) "Program runs!"
 !contains
 
